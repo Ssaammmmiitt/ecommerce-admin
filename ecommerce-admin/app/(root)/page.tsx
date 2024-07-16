dfb825505e3d4911c6daaa936f78b28a6d0302f3
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-07-16 22:25:11 +0545

    chore: Update page.tsx to use lazy loading for images

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,8 +1,17 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
+import { UseStoreModal } from "@/hooks/use-store-modal";
+import { useEffect } from "react";
 
 const SetupPage = () => {
+  const onOpen = UseStoreModal((state)=>state.onOpen);
+  const isOpen=UseStoreModal((state)=>state.isOpen);
+
+  useEffect(()=>{
+    if(!isOpen){
+      onOpen();
+    }
+  },[isOpen,onOpen]);
   return <div className="p-4">Root Page </div>;
 };
 
