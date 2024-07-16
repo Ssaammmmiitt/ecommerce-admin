79efb7194e0443186c451d95585a8ab86797f693
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-07-16 19:13:35 +0545

    chore: Add @radix-ui/react-dialog and @radix-ui/react-slot dependencies
    Updated Authentication

--- ecommerce-admin/app/(root)/page.tsx
+++ ecommerce-admin/app/(root)/page.tsx
@@ -1,9 +1,12 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage =() => {
   return (
     <div className="p-4">
-        <Button size="default" >Click Me!</Button>
+        <p>This is a protected Route:!</p>
+        <UserButton afterSignOutUrl="/"/>
     </div>
 );
 }
+
+export default SetupPage;
--- ecommerce-admin/app/layout.tsx
+++ ecommerce-admin/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,9 +13,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <ClerkProvider>
     <html lang="en">
