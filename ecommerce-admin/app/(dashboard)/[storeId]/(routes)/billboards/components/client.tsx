e5fb011ebb00936404b188942078bd55804afdf6
Sammit Poudyal <poudyal.sammit@gmail.com>
2024-07-20 03:09:20 +0545

    Managed API routes in billboards page

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -9,6 +9,7 @@ import { Separator } from "@/components/ui/separator";
 import { useParams, useRouter } from "next/navigation";
 import { BillboardColumn, columns } from "./columns";
 import { DataTable } from "@/components/ui/data-table";
+import { ApiList } from "@/components/ui/api-list";
 
 interface BillboardClientProps {
   data: BillboardColumn[];
@@ -33,6 +34,10 @@ export const BillboardClient:React.FC<BillboardClientProps> = ({
       </div>
       <Separator />
       <DataTable searchKey="label" columns={columns} data={data}/>
+      <Heading title="API" description="API calls for Billboards"/>
+      <Separator/>
+      <ApiList entityIdName="billboardId" entityName="billboards"/>
+    
     </>
   );
 };
